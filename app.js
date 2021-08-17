f5ea3cdb2c876f3db91dacb548378716eef58c7a
NtshVrm <nitishvarma2k@gmail.com>
2021-08-17 08:26:20 +0530

    files: initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+var btnTranslate = document.querySelector("#btn-translate");
+
+var txtInput = document.querySelector("#txt-input");
+
+var outputDiv = document.querySelector("#output-box");
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text) {
+
+    return serverURL + "?" + "text=" + text;
+
+}
+
+function errorHandler(error){
+    console.log("Error Occured", error);
+    alert("Server Down, please try again later!");
+}
+
+
+
+
+btnTranslate.addEventListener("click", function clickEventHandler() {
+
+    // outputDiv.innerText = "TRANSLATED TEXT" + " from " + txtInput.value;
+
+    //input text
+
+    var inputText = txtInput.value; 
+
+    //server call for processing
+
+    fetch(getTranslationURL(inputText)).then(response => response.json()).then(json => {var translatedText = json.contents.translated; outputDiv.innerText = translatedText}).catch(errorHandler)
+
+})
